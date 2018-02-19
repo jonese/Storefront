e581205623842ad0283cd0ab492ff44f79132416
Ed Jones <epj615@gmail.com>
2018-02-18 22:13:49 -0600

    Pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-main',
+  template: '<store></store>'
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent { }
